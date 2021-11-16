46fada0874b7e19abc854e9682d8f686f616c4ef
numan <numan856@gmail.com>
2021-11-15 20:41:24 -0800

    feat: Wrap all components into a parent stack

--- app.py
+++ app.py
@@ -1,15 +1,65 @@
 #!/usr/bin/env python3
 import os
 
-from aws_cdk import core
+from aws_cdk import core as cdk
 
 from yearn_simulations_infra.yearn_simulations_infra_stack import (
-    YearnSimScheduledTasksInfraStack,
-    YearnSimulationsInfraStack,
-    SharedStack,
-)
+    SharedStack, YearnSimScheduledTasksInfraStack, YearnSimulationsInfraStack)
 
-app = core.App()
+app = cdk.App()
+
+
+class ApplicationStack(cdk.Stack):
+    def __init__(
+        self, scope: cdk.Construct, construct_id: str, vpc_id: str, **kwargs
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        shared_stack = SharedStack(
+            self,
+            "SharedStack",
+            env=cdk.Environment(
+                account=os.environ.get(
+                    "CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]
+                ),
+                region=os.environ.get(
+                    "CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]
+                ),
+            ),
+        )
+
+        YearnSimulationsInfraStack(
+            self,
+            "YearnSimulationsInfraStack",
+            vpc_id=vpc_id,
+            log_group=shared_stack.log_group,
+            container_repo=shared_stack.container_repo,
+            env=cdk.Environment(
+                account=os.environ.get(
+                    "CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]
+                ),
+                region=os.environ.get(
+                    "CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]
+                ),
+            ),
+        )
+
+        YearnSimScheduledTasksInfraStack(
+            self,
+            "YearnSimScheduledTasksInfraStack",
+            vpc_id=vpc_id,
+            log_group=shared_stack.log_group,
+            container_repo=shared_stack.container_repo,
+            env=cdk.Environment(
+                account=os.environ.get(
+                    "CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]
+                ),
+                region=os.environ.get(
+                    "CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]
+                ),
+            ),
+        )
+
 
 vpc_id = os.environ.get("CDK_DEPLOY_VPC", None)
 if not vpc_id:
@@ -17,37 +67,8 @@ if not vpc_id:
         "Can not deploy without an existing VPC. Please specify which VPC you want to deploy to using the `CDK_DEPLOY_VPC` environment variable."
     )
 
-shared_stack = SharedStack(
-    app,
-    "SharedStack",
-    env=core.Environment(
-        account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
-        region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]),
-    ),
-)
-
-YearnSimulationsInfraStack(
-    app,
-    "YearnSimulationsInfraStack",
-    vpc_id=vpc_id,
-    log_group=shared_stack.log_group,
-    container_repo=shared_stack.container_repo,
-    env=core.Environment(
-        account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
-        region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]),
-    ),
-)
-
-YearnSimScheduledTasksInfraStack(
-    app,
-    "YearnSimScheduledTasksInfraStack",
-    vpc_id=vpc_id,
-    log_group=shared_stack.log_group,
-    container_repo=shared_stack.container_repo,
-    env=core.Environment(
-        account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
-        region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]),
-    ),
-)
+prod = ApplicationStack(app, "Production", vpc_id)
+# staging = ApplicationStack(app, "Staging", staging_vpc_id)
+# dev = ApplicationStack(app, "Dev", dev_vpc_id)
 
 app.synth()
